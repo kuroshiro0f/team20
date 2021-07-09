712e60de7494761ae5004aa905ce0fdbeb1d6b53
yutaro ono <ono.yutaro.tec0623@gmail.com>
2021-07-09 21:02:45 +0900

    playerの軌道エフェクト、難易度選択に対応したタイトルシーンを追加

---
+++ TestTitleSceneOno.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "SceneBase.h"
+
+
+class TestTitleScene : public SceneBase
+{
+public:
+
+	enum class TITLE_TRANS_STATE
+	{
+		FIRST_ENTER = 0,
+		SECOND_CHOICE
+	};
+
+	TestTitleScene();
+	~TestTitleScene();
+
+	SceneBase* Update() override;
+	void Draw() override;
+	void Sound() override;
+	void Load() override;
+
+	void UpdateTransparent();
+
+private:
+
+	TITLE_TRANS_STATE m_state;
+
+	unsigned int m_cursolNum;    // 難易度選択時のカーソル番号
+
+	int m_backGraphHandle;				//	タイトル画面・背景のグラフィックハンドル
+	int m_logoGraphHandle;				//	タイトル画面・ロゴのグラフィックハンドル
+	int m_guidanceGraphHandle;			//	タイトル画面・ガイドのグラフィックハンドル
+	int m_soundHandle;					//	タイトル画面・サウンドハンドル
+	// 毎フレーム透過量
+	int m_permeationAmount;
+	// 透過量
+	int m_transpVal;
+
+	bool m_checkKeyFlag;				//	キーが押されたままかを判定するフラグ
+};
